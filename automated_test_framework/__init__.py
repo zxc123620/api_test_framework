e7502aa7ede3c42e381c91002dd0576a419e8b77
一只踩坑少年 <3067188237@qq.com>
2025-08-10 20:55:41 +0800

    1、更新配置文件加载方式 2、已打包

--- automated_test_framework/__init__.py
+++ automated_test_framework/__init__.py
@@ -3,6 +3,9 @@
 # @FileName  :__init__.py.py
 # @Time      :2025/8/3 22:50
 # @Author    :zhouxiaochuan
-# @Description: 
+# @Description:
 import automated_test_framework.logger_config
+import automated_test_framework.load_config
+# load_config.init_config()
+
 
--- automated_test_framework/load_config.py
+++ automated_test_framework/load_config.py
@@ -64,15 +64,22 @@ while True:
     else:
         dir_path, file_path = os.path.split(temp_dir)
         temp_dir = dir_path
+global_config_obj = None
 
-with open(config_path, 'r', encoding='utf-8') as f:
-    result = yaml.load(f.read(), Loader=yaml.FullLoader)
-    logging.info(f"加载配置文件:{result}")
-    sub_class = GlobalConfig.__subclasses__()
-    if sub_class:
-        global_config_obj = sub_class[0](**result)
-    else:
-        global_config_obj = GlobalConfig(**result)
-    # GlobalConfig.YML_CONFIG_DATA = result
+def init_config():
+    global global_config_obj
+    if global_config_obj is not None:
+        return
+    with open(config_path, 'r', encoding='utf-8') as f:
+        result = yaml.load(f.read(), Loader=yaml.FullLoader)
+        logging.info(f"加载配置文件:{result}")
+        sub_class = GlobalConfig.__subclasses__()
+        if sub_class:
+            logging.info(f"检测到子类,使用子类:{sub_class[0]}")
+            global_config_obj= sub_class[0](**result)
+        else:
+            logging.info("使用默认类")
+            global_config_obj =  GlobalConfig(**result)
+
 
 logging.info(global_config_obj)
